bed09fc8477bd2c998e0d224ce81b7195639ba42
Jayachandranm46 <158546430+Jayachandranm46@users.noreply.github.com>
2024-08-26 13:55:10 +0530

    changes

---
+++ src/common/Imagemapping.js
@@ -0,0 +1,12 @@
+
+const imageMapping = {
+    1: require('../../assets/fortune.png'),
+    2: require('../../assets/Almonds.png'),
+    3: require('../../assets/Garlic.png'),
+    4: require('../../assets/OrganicSpices.png'),
+    5: require('../../assets/MixedVegetables.png'),
+    6: require('../../assets/RoastedPeas.png'),
+    // Add other mappings as needed
+};
+
+export default imageMapping;
